69193c0587139c984b982743d984aa4ed9e7551f
rodlucenac <digolucena14@gmail.com>
2023-11-26 21:46:28 -0300

    update staticfiles

--- projeto_Synthesi/projeto_Synthesi/settings.py
+++ projeto_Synthesi/projeto_Synthesi/settings.py
@@ -143,6 +143,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
