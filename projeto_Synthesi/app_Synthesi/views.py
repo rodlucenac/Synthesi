81e41e8e632a1fab76b9cfef4e74668baeed9fee
Souto96 <lucasoutom@gmail.com>
2023-11-28 21:22:33 -0300

    update templates

--- projeto_Synthesi/app_Synthesi/views.py
+++ projeto_Synthesi/app_Synthesi/views.py
@@ -54,8 +54,10 @@ def pagina_cadastrog(request):
 def pagina_salas(request):
     return render(request, 'salas.html')
 
-def pagina_monitoramento(request):
-    return render(request, 'monitoramento.html')
+def pagina_monitoramento(request, nome, turma, idade):
+    print(nome, turma, idade)
+    context = {'nome': nome, 'turma': turma, 'idade': idade}
+    return render(request, 'monitoramento.html', context)
 
 def pagina_presenca(request):
     return render(request, 'presenca.html')
@@ -66,8 +68,24 @@ def pagina_musica(request):
 def pagina_autoavaliacao(request):
     return render(request, 'autoavaliacao.html')
 
+def pagina_atividades(request, nome=None, turma=None, idade=None):
+    if nome is None or turma is None or idade is None:
+        # Se os argumentos não foram fornecidos, você pode buscar essas informações de outra forma.
+        # Por exemplo, você pode buscar essas informações do banco de dados ou usar dados padrão.
+        # Aqui estou apenas dando um exemplo fixo, ajuste conforme necessário.
+        nome = "Aluno Padrão"
+        turma = "Turma Padrão"
+        idade = 10
+    print(nome, turma, idade)
+    context = {'nome': nome, 'turma': turma, 'idade': idade}
+    return render(request, 'atividades.html', context)
+
+
 def pagina_eueomundo(request):
-    alunos = Alunos.objects.all()  # Obtém todos os alunos do banco de dados
+    alunos = Alunos.objects.all() 
+    for i, aluno in enumerate(alunos):
+        aluno.top = 150 + (i // 4) * 200  # Ajuste os valores conforme necessário
+        aluno.left = 20 + (i % 4) * 200   # Ajuste os valores conforme necessário 
     return render(request, 'eu_eo_mundo.html', {'alunos': alunos})
 
 def pagina_adicionar(request):
