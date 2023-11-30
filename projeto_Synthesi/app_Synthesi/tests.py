f5278564452c0605045b6232229c2f56059177fa
Souto96 <lucasoutom@gmail.com>
2023-11-30 11:47:48 -0300

    update tests

--- projeto_Synthesi/app_Synthesi/tests.py
+++ projeto_Synthesi/app_Synthesi/tests.py
@@ -1,26 +1,147 @@
-import unittest
+from django.test import TestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.support.ui import Select
+import random
+import string
+import time
+import os
+import tempfile
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
-class SalasPageTest(StaticLiveServerTestCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.driver = webdriver.Chrome('/path/to/chromedriver')
-        cls.driver.implicitly_wait(10)
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.driver.quit()
-        super().tearDownClass()
 
-    def test_elements_on_salas_page(self):
-        self.driver.get(f'{self.live_server_url}/caminho/para/salas')
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-gpu")
+browser = webdriver.Chrome(options=chrome_options)
 
-        self.assertTrue(self.driver.find_element(By.ID, "salas").is_displayed())
-        self.assertTrue(self.driver.find_element(By.ID, "folha_salas").is_displayed())
-        self.assertTrue(self.driver.find_element(By.ID, "elipse1").is_displayed())
 
-if __name__ == '__main__':
-    unittest.main()
+wait = WebDriverWait(browser, 10)
+
+username = ''.join(random.choices(string.ascii_lowercase, k=8))
+password = ''.join(random.choices(string.ascii_letters + string.digits, k=12))
+
+class SalasPageTest(TestCase):
+    def test_a_salas(self):
+        browser.get("http://127.0.0.1:8000/salas/")
+        time.sleep(5)
+        from django.test import TestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import random
+import string
+import time
+import os
+import tempfile
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--headless")
+browser = webdriver.Chrome(options=chrome_options)
+
+
+wait = WebDriverWait(browser, 10)
+
+username = ''.join(random.choices(string.ascii_lowercase, k=8))
+password = ''.join(random.choices(string.ascii_letters + string.digits, k=12))
+
+class SalasPageTest(TestCase):
+    def test_a_salas(self):
+        browser.get("http://127.0.0.1:8000/salas/")
+        time.sleep(5)
+
+    
+    
+    
+
+        
+from django.test import TestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import random
+import string
+import time
+import os
+import tempfile
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from django import forms 
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-gpu")
+browser = webdriver.Chrome(options=chrome_options)
+
+
+wait = WebDriverWait(browser, 10)
+
+username = ''.join(random.choices(string.ascii_lowercase, k=8))
+password = ''.join(random.choices(string.ascii_letters + string.digits, k=12))
+my_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+
+class MyForm(forms.Form):
+    my_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+
+
+class SalasPageTest(TestCase):
+    def test_a_salas(self):
+        browser.get("http://127.0.0.1:8000/salas/")
+        time.sleep(2)
+        browser.find_element(By.ID, "icn1").click()
+        time.sleep(2)
+        assert browser.current_url == "http://127.0.0.1:8000/eu_eo_mundo/"
+    
+    
+    def test_b_atividades(self):
+        browser.get("http://127.0.0.1:8000/monitoramento/messi/manha/8/")
+        time.sleep(3)
+        browser.find_element(By.ID, "autoavaliacao").click()
+        time.sleep(3)
+        browser.find_element(By.ID, "enviar").click()
+        time.sleep(2)
+        assert browser.current_url == ("http://127.0.0.1:8000/monitoramento/messi/manha/8/")
+    
+    
+    def test_c_autoavaliacao(self):
+        browser.get("http://127.0.0.1:8000/monitoramento/messi/manha/8/")
+        time.sleep(3)
+        browser.find_element(By.ID, "reunioes").click()
+        time.sleep(3)
+        browser.find_element(By.ID, "solicitar").click()
+        time.sleep(2)
+        browser.find_element(By.ID, "mensagem_solicitacao").send_keys(password)
+        time.sleep(2)
+        browser.find_element(By.ID, "enviar").click()
+        time.sleep(2)
+    
+        assert browser.current_url == "http://127.0.0.1:8000/solicitar/messi/manha/8/"
+
+    
+    
+
+
+
+    
+
+
+
+
+        
+
+    
+    
+    
+
+        
